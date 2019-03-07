86d3cc0bd4630526c899891edf2df47048fac207
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-03-07 14:58:35 -0600

    formatting

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+
 //Load user model
 const User = require("../../models/User");
 

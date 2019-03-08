6d2a36c96dbe9d1b9a1124fbc78d7208f25a4754
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-03-08 08:59:19 -0600

    added passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load user model
 const User = require("../../models/User");
@@ -69,17 +70,37 @@ router.post("/login", (req, res) => {
         //User matched
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; //create jwt payload
         //sign token
-        jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
-          res.json({
-            success: true,
-            token: "Bearer " + token
-          });
-        });
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password Incorrect" });
       }
     });
   });
 });
+
+//@route GET api/users/current
+//@dsc Return current user
+//@access Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -20,10 +21,13 @@ mongoose
   .then(() => console.log("mongoDB connected"))
   .catch(err => console.log(error));
 
-app.get("/", (req, res) => res.send("Welcome"));
+//Passport middleware
+app.use(passport.initialize());
 
-//use routes
+//Passport config
+require("./config/passport")(passport);
 
+//Use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);

6949d2e755d57d5be26f23aabc49151a2a958263
Ben Gimbel <BenGimbel@Bens-MacBook-Pro.local>
2019-03-09 14:07:26 -0600

    added delete route for post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,9 @@ const passport = require("passport");
 //Post Model
 const Post = require("../../models/Post");
 
+//Profile Model
+const Profile = require("../../models/Profile");
+
 //Validation
 const validatePostInput = require("../../validation/post");
 
@@ -13,6 +16,27 @@ const validatePostInput = require("../../validation/post");
 //@dsc Tests posts route
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+
+//@route GET api/posts
+//@dsc Get Post
+//@access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ noPostsFound: "No posts found" }));
+});
+
+//@route GET api/posts/:id
+//@dsc Get Post by id
+//@access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ noPostFound: "No post found with that ID" })
+    );
+});
 
 //@route POST api/posts
 //@dsc Create Post
@@ -38,6 +62,32 @@ router.post(
 
     newPost.save().then(post => {
       res.json(post);
+    });
+  }
+);
+
+//@route DELETE api/posts/:id
+//@dsc Delete post
+//@access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        //Check for post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notAuthorized: "User not authorized" });
+        }
+
+        //Delete
+        post
+          .remove()
+          .then(() => res.json({ success: true }))
+          .catch(err =>
+            res.status(404).json({ postNotFound: "No post found" })
+          );
+      });
     });
   }
 );
